49a6a76d37e029963147cafc464025481e2b31b3
Pedro Santana <97036217+pedrohsantanaa@users.noreply.github.com>
2025-04-08 21:34:09 -0300

    Versão Final

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Tarefas(models.Model):
@@ -14,7 +15,7 @@ class Tarefas(models.Model):
         max_length=9,
         choices= STATUS,
     )
-
+    usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
--- tarefas/views.py
+++ tarefas/views.py
@@ -7,12 +7,12 @@ from .forms import TarefaForm
 
 @login_required
 def listaTarefa(request):
-    tarefas_list = Tarefas.objects.all().order_by('-created_at')
+    tarefas_list = Tarefas.objects.all().order_by('-created_at').filter(usuario=request.user)
 
     search = request.GET.get('search')
 
     if search:
-        tarefas = Tarefas.objects.filter(titulo__icontains=search)
+        tarefas = Tarefas.objects.filter(titulo__icontains=search, usuario=request.user)
         return render(request, 'tarefas/list.html', {'tarefas':tarefas})
     
     else:
@@ -32,6 +32,7 @@ def novaTarefa(request):
         if form.is_valid():
             tarefa = form.save(commit=False)
             tarefa.status = 'andamento'
+            tarefa.usuario = request.user
             tarefa.save()
             return redirect('/')
 
